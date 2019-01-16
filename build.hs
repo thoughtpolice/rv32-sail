805e5bac704b60a68b3d9ae71a4d0e5b4eec9949
Austin Seipp <aseipp@pobox.com>
2019-01-16 11:27:02 -0600

    t: add basic test firmware
    Copied from picorv32; the tests themselves come from the upstream RISC-V
    tests.
    
    This will need many modifications to compile, even after the simulator
    is working. But the build system to produce ELF images works right now.
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- build.hs
+++ build.hs
@@ -50,13 +50,87 @@ specSources
 specSources mainFile = pure $ map (\x -> "src/spec" </> x <.> "sail")
   [ "setup", "basics", "scattered", fromMaybe "elfmain" mainFile ]
 
--- | Top-level set of all rules for the build system.
-mainRules :: Rules ()
-mainRules = do
+-- | Top-level set of all rules for building the emulator.
+emulatorRules :: Rules ()
+emulatorRules = do
   -- main emulator: 'cruise' C backend, with default ELF loader
   specSources Nothing >>= sailSimRules SailBackendC (bdir "cruise")
 
-  -- TODO FIXME: build firmware source code
+--------------------------------------------------------------------------------
+-- Tests
+
+getInstrTestFiles :: Action [FilePath]
+getInstrTestFiles = getDirectoryFiles "src" [ "/t/tests/*.S" ]
+
+getFirmwareTestFiles :: Action [FilePath]
+getFirmwareTestFiles = getDirectoryFiles "src"
+  [ "/t/firmware/*.c", "/t/firmware/*.S" ]
+
+testRules :: Rules ()
+testRules = do
+
+  let takeBaseName1 = dropExtensions . takeBaseName
+
+  -- assembles the files in ./src/t/tests/
+  let tasm out name = do
+        let src = "src/t/tests" </> name <.> "S"
+        need [ src ]
+        cmd "riscv32-unknown-elf-gcc -march=rv32im"
+          [ "-c", "-o", out ]
+          ("-DTEST_FUNC_NAME=" <> name)
+          ("-DTEST_FUNC_TXT=" <> show name)
+          ("-DTEST_FUNC_RET=" <> name <> "_ret")
+          src
+
+  -- links rv32 objects together
+  let rvld out objs = do
+        let ldsect = "src/t/firmware/sections.lds"
+            ldmap  = "src/t/firmware/firmware.map"
+            flags  = [ "-Wl,-Bstatic,-T,", ldsect, ",-Map,", ldmap, ",--strip-debug" ]
+
+        need (ldsect:objs)
+        cmd "riscv32-unknown-elf-gcc"
+          [ "-ffreestanding", "-nostdlib" ]
+          [ "-o", out ]
+          (concat flags)
+          objs
+          [ "-lgcc" ]
+
+  -- compiles C firmware test files
+  let rvcc out name = do
+        let src = "src/t/firmware" </> name <.> "c"
+
+        need [ src ]
+        cmd "riscv32-unknown-elf-gcc -march=rv32ic"
+          [ "-Os", "-pedantic", "-std=c99" ]
+          [ "-Werror", "-Wall", "-Wextra", "-Wshadow", "-Wundef"
+          , "-Wpointer-arith", "-Wcast-qual", "-Wcast-align", "-Wwrite-strings"
+          , "-Wredundant-decls", "-Wstrict-prototypes", "-Wmissing-prototypes"
+          ]
+          [ "-ffreestanding", "-nostdlib" ]
+          [ "-c", "-o", out, src ]
+
+  -- assembles firmware test files
+  let rvasm out name = do
+        let src = "src/t/firmware" </> name <.> "S"
+        need [ src ]
+        cmd "riscv32-unknown-elf-gcc -march=rv32imc"
+          [ "-c", "-o", out, src ]
+
+  bdir "selftest.elf" %> \out -> do
+    asm_srcs <- getInstrTestFiles
+    fw_srcs <- getFirmwareTestFiles
+
+    let asm_objs = [ bdir v -<.> "asm.o" | v <- asm_srcs ]
+        fw_objs  = [ bdir v -<.> "asm.o" | v <- fw_srcs, takeExtensions v == ".S" ]
+                <> [ bdir v -<.> "c.o" | v <- fw_srcs, takeExtensions v == ".c" ]
+
+    rvld out (fw_objs <> asm_objs)
+
+  -- Test rules
+  bdir "t/tests/*.asm.o" %> \out -> tasm out (takeBaseName1 out)
+  bdir "t/firmware/*.asm.o" %> \out -> rvasm out (takeBaseName1 out)
+  bdir "t/firmware/*.c.o"   %> \out -> rvcc  out (takeBaseName1 out)
 
 --------------------------------------------------------------------------------
 -- Driver
@@ -67,7 +141,9 @@ main = shakeArgsWith myShakeOptions myFlags $ \_ targets -> pure $ Just $ do
   want (if not (null targets) then targets else [ "all" ])
 
   -- phony rules which are shorthands
-  "all" ~> need [ bdir "cruise" ]
+  "all" ~> need [ bdir "cruise", bdir "selftest.elf" ]
   "clean" ~> removeFilesAfter (bdir mempty) ["//*"]
 
-  mainRules
+  -- all main, top-level rule sets
+  emulatorRules
+  testRules
