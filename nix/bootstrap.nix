2be4aca0f016a9058cfd9ee5bc7e80cf932ebb16
Austin Seipp <aseipp@pobox.com>
2019-01-30 15:30:21 -0600

    nix: gate minimum nix version
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- nix/bootstrap.nix
+++ nix/bootstrap.nix
@@ -4,8 +4,14 @@
 }:
 
 let
+  minimumNixVersion = "2.1";
+
   json = with builtins; fromJSON (readFile ./nixpkgs.json);
   pkgs = if nixpkgs != null then nixpkgs else builtins.fetchTarball {
     inherit (json) url sha256;
   };
-in import pkgs  { inherit config system; }
+in
+with builtins;
+  if (compareVersions nixVersion minimumNixVersion) >= 0
+    then import pkgs { inherit config system; }
+    else throw ("Invalid Nix version '" + nixVersion + "'; at least " + minimumNixVersion + " is required!")
