400cebe0dab4e2572950249570ada9be0b86a30d
Austin Seipp <aseipp@pobox.com>
2019-01-30 08:56:37 -0600

    libfirm: minor fix to string.c includes
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- src/libfirm/string.c
+++ src/libfirm/string.c
@@ -1,5 +1,5 @@
-#include "stddef.h"
-#include "string.h"
+#include <stddef.h>
+#include <string.h>
 
 int strcmp(const char *s1, const char *s2) {
   while (*s1 && *s2 && *s1 == *s2) { s1++; s2++; }
